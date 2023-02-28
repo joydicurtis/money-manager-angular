f64ada37fad75e4d9cdb2177ae58398d61c1569f
Yevheniia Nezhalska <joydicurtis@gmail.com>
2023-02-28 22:03:50 +0200

    layout

---
+++ src/app/incomings/incomings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incomings',
+  templateUrl: './incomings.component.html',
+  styleUrls: ['./incomings.component.scss']
+})
+export class IncomingsComponent {
+
+}
