a85e821de254ec70c62143cade102e90b6e76bb3
Yevheniia Nezhalska <joydicurtis@gmail.com>
2023-03-01 23:05:05 +0200

    form

--- src/app/manage-dialog/manage-dialog.component.ts
+++ src/app/manage-dialog/manage-dialog.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DialogService } from '../services/dialog.service';
+import { incomingsCategories } from './categories.const';
 @Component({
   selector: 'app-manage-dialog',
   templateUrl: './manage-dialog.component.html',
-  styleUrls: ['./manage-dialog.component.scss']
+  styleUrls: ['./manage-dialog.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    'class': 'dialog'
+  }
 })
-export class ManageDialogComponent {
+export class ManageDialogComponent implements OnInit {
+  constructor(public dialogService: DialogService, private fb:FormBuilder) {
+    this.incomingsCategories = incomingsCategories;
+  }
+  dateControl!: FormControl;
+  incomingsCategories: any;
+  operationFormGroup!: FormGroup;
+  isSubmitted: boolean = false;
+  ngOnInit() {
+    this.operationFormGroup = this.fb.group({
+      sumControl: this.fb.control('', [Validators.required, Validators.pattern(/^[1-9]\d*([\,\.]\d{2})?$/)]),
+      dateControl: this.fb.control(new Date(), Validators.required),
+      noteControl: this.fb.control(''),
+      categories: this.fb.control('', Validators.required)
+    });
+  }
 
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.operationFormGroup.valid) {
+      return this.operationFormGroup.value;
+    }
+  }
 }
