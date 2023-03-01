147c39359756e58dc7ff4e565bb5168b506bc4ee
Yevheniia Nezhalska <joydicurtis@gmail.com>
2023-03-01 19:59:08 +0200

    firebase settings

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'money-manager-21fd9',
+    appId: '1:349110067386:web:363b35564a6f70ad486283',
+    storageBucket: 'money-manager-21fd9.appspot.com',
+    apiKey: 'AIzaSyDLYHOUJKvJwl7V3LtFM9cZ8xVz57L53eg',
+    authDomain: 'money-manager-21fd9.firebaseapp.com',
+    messagingSenderId: '349110067386',
+    measurementId: 'G-46VVKDFQ6M',
+  },}
