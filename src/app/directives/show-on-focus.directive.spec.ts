bf7c523b4954e7aca753e72cf25b518dc8071263
Yevheniia Nezhalska <joydicurtis@gmail.com>
2023-03-24 13:39:14 +0200

    focus directive

---
+++ src/app/directives/show-on-focus.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShowOnFocusDirective } from './show-on-focus.directive';
+
+describe('ShowOnFocusDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShowOnFocusDirective();
+    expect(directive).toBeTruthy();
+  });
+});
