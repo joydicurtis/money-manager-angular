b2884025dae8051a5d0b53d6b184bc1d0dd5fd17
Yevheniia Nezhalska <joydicurtis@gmail.com>
2023-04-04 02:11:40 +0300

    firebase upd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,19 @@
 export const environment = {
   firebase: {
-    projectId: 'money-manager-21fd9',
-    appId: '1:349110067386:web:363b35564a6f70ad486283',
-    storageBucket: 'money-manager-21fd9.appspot.com',
-    apiKey: 'AIzaSyDLYHOUJKvJwl7V3LtFM9cZ8xVz57L53eg',
-    authDomain: 'money-manager-21fd9.firebaseapp.com',
-    messagingSenderId: '349110067386',
-    measurementId: 'G-46VVKDFQ6M',
+    projectId: "money-manager-angular-9088f",
+    appId: "1:426904021899:web:71b9b04799dd3f383c5d8e",
+    storageBucket: "money-manager-angular-9088f.appspot.com",
+    apiKey: "AIzaSyAdzPkhD9OMtdQ25qvWzU9iewVADUZUYGA",
+    authDomain: "money-manager-angular-9088f.firebaseapp.com",
+    messagingSenderId: "426904021899",
+    measurementId: "G-WPFHK4L0D0"
   },
 };
+
+
+
+
+
+
+
+
