e5cb6c6c38336367922c2ee07d26482e27dceb43
Yevheniia Nezhalska <joydicurtis@gmail.com>
2023-07-04 14:51:35 +0300

    custom library

--- src/app/converter/converter.module.ts
+++ src/app/converter/converter.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ConverterComponent } from './converter/converter.component';
 import { RouterModule } from '@angular/router';
 import { ConverterRoutingModule } from './converter-routing.module'
-
+import { CurrencyConverterLibModule } from 'currency-converter-lib'
 @NgModule({
   declarations: [
     ConverterComponent
   ],
   imports: [
     CommonModule,
-    ConverterRoutingModule
+    ConverterRoutingModule,
+    CurrencyConverterLibModule
   ],
   exports: [RouterModule]
 })
